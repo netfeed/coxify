f78898f3e946e41a231f0c98238846e4d1f2e5ad
Victor Bergöö <victor.bergoo@gmail.com>
2011-12-21 22:45:12 +0100

    index should be the latest day

--- lib/Coxify/Controller/Index.pm
+++ lib/Coxify/Controller/Index.pm
@@ -10,12 +10,26 @@ use Coxify::Model::Image;
 sub index {
   my ($self) = @_;
 
+  my ($sql, $binds) = Coxify::Model::Image::Manager->get_objects_sql(
+    select => 'created_date',
+    where => [
+      active => 1,
+    ],
+    sort_by => 'created_date DESC',
+    group_by => 'created_date',
+    limit => 1
+  );
+
+  my $db = new Coxify::Db();
+  my $dbh = $db->dbh;
+  my $query = $dbh->selectall_arrayref($sql, {}, @{ $binds });
+
   my $images = Coxify::Model::Image::Manager->get_images(
     query => [
       active => 1,
+      created_date => $query->[0]->[0]
     ],
     sort_by => 'id DESC',
-    limit => '24'
   );
 
   my $year_list = Coxify::Image::year_list($self->stash('year'));
